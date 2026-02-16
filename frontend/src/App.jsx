de819ab858027b077fba649c7d4ae8779167169b
Ivan Shumeiko <ivan.shumeiko24@gmail.com>
2026-02-16 18:05:52 +0300

    create marker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import MapComponent from './Map'
 function App() {
   return (
     <div>
-      <h1>Traveler Map</h1>
       <MapComponent />
     </div>
   )
--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -3,23 +3,61 @@ import Map from "ol/Map"
 import View from "ol/View"
 import TileLayer from "ol/layer/Tile"
 import OSM from "ol/source/OSM"
+import VectorLayer from "ol/layer/Vector"
+import VectorSource from "ol/source/Vector"
+import Feature from "ol/Feature"
+import Point from "ol/geom/Point"
+import { Style, Icon } from "ol/style"
+import { fromLonLat, toLonLat } from "ol/proj"
 import "ol/ol.css"
 
 export default function MapComponent() {
     const mapRef = useRef()
+    const vectorSourceRef = useRef(new VectorSource())
 
     useEffect(() => {
+        const vectorLayer = new VectorLayer({
+            source: vectorSourceRef.current
+        })
+
         const map = new Map({
             target: mapRef.current,
             layers: [
                 new TileLayer({
                     source: new OSM()
-                })
+                }),
+                vectorLayer
             ],
             view: new View({
                 center: [0, 0],
                 zoom: 2
             })
+        })
+
+        // Обработчик клика на карте
+        map.on("click", (event) => {
+            const coordinates = event.coordinate
+            const lonlat = toLonLat(coordinates)
+
+            console.log("Координаты:", lonlat)
+
+            // Очистка старого маркера
+            vectorSourceRef.current.clear()
+
+            const marker = new Feature({
+                geometry: new Point(coordinates)
+            })
+
+            marker.setStyle(
+                new Style({
+                    image: new Icon({
+                        src: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+                        scale: 0.05
+                    })
+                })
+            )
+
+            vectorSourceRef.current.addFeature(marker)
         })
 
         return () => map.setTarget(null)
@@ -28,7 +66,7 @@ export default function MapComponent() {
     return (
         <div
         ref={mapRef}
-        style={{ height: '500px', width: '100%' }}
+        style={{ height: '100vh', width: '100%' }}
         />
     )
 }
